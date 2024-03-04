74609244c5a1e9bdbfb0a5679601d66b5c10d614
rhenanbartels <rhenan.bartels@gmail.com>
2024-03-04 15:01:27 -0300

    Add methods to add and delete data

--- afpgvector/models.py
+++ afpgvector/models.py
@@ -1,3 +1,5 @@
+from hashlib import md5
+
 from django.conf import settings
 from django.db import models
 from pgvector.django import CosineDistance, VectorField
@@ -28,3 +30,21 @@ class Document(models.Model):
         ).defer("embedding", "hash_id", "created_at").filter(
             distance__lte=score_threshold
         ).order_by("distance")[:n_results]
+
+    @classmethod
+    def insert(cls, embedding, content, metadata):
+        print("Inserting data on Postgres Vector DB...")
+        hash_id = md5(content.encode()).hexdigest()
+        return cls.objects.using("vector").create(
+            content=content,
+            embedding=embedding,
+            metadata=metadata,
+            hash_id=hash_id,
+        )
+
+    @classmethod
+    def delete_from_url(cls, url):
+        documents = cls.objects.using("vector").filter(metadata__url=url)
+        print(f"Deleting {documents.count()}...")
+        documents.delete()
+        print("Done!")
